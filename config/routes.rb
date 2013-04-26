a396297a4559d0d233dbaa74d91abff3413efcbc
Tracy Chung <tracychung@me.com>
2013-04-26 01:53:35 -0400

    Add Pins Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Omrails::Application.routes.draw do
 
   root :to => 'pages#home'
 
-  get :pins
+  resources :pins
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
